f93196620c810581137f0f2db4f828bc11047fd2
SGaikwad3 <sgaikwad3@slb.com>
2020-03-27 22:49:28 +0530

    urls added

--- drf_hello_world/urls.py
+++ drf_hello_world/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from drf_api.views import hello_world_api
+
 
 urlpatterns = [
+    url(r'^hello-world-api/$', hello_world_api.as_view()),
     path('admin/', admin.site.urls),
 ]
